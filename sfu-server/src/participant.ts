5ef86078c102a959c883bfe52d06556b0f84dd12
Kick Buttowski <kickbuttowski@gmail.com>
2025-02-03 18:45:24 +0530

    feat: added sfu server lib

---
+++ sfu-server/src/participant.ts
@@ -0,0 +1,171 @@
+import { RtpCapabilities, RtpParameters } from "mediasoup/node/lib/types.js";
+import {
+  DtlsParameters,
+  WebRtcTransport,
+} from "mediasoup/node/lib/WebRtcTransportTypes.js";
+import {
+  Room,
+  SFUConsumer,
+  SFUProducer,
+  TrackSource,
+  TransportDirection,
+  Transports,
+} from "./types.js";
+
+export class Participant {
+  private id: string;
+
+  private transports: {
+    send: WebRtcTransport;
+    recv: WebRtcTransport;
+  };
+
+  private room: Room;
+
+  private consumers: Record<string, SFUConsumer> = {};
+  private producers: Record<string, SFUProducer> = {};
+
+  private rtpCapabilities: RtpCapabilities | null = null;
+
+  constructor(id: string, transports: Transports, room: Room) {
+    this.id = id;
+    this.transports = transports;
+    this.room = room;
+  }
+
+  connectTransport(
+    direction: TransportDirection,
+    dtlsParameters: DtlsParameters,
+  ) {
+    const transport = this.transports[direction];
+    transport.connect({ dtlsParameters });
+  }
+
+  setRTPCapabilities(rtpCapabilities: RtpCapabilities) {
+    this.rtpCapabilities = rtpCapabilities;
+  }
+
+  async produce(source: TrackSource, rtpParameters: RtpParameters) {
+    const transport = this.transports.send;
+    const producer = await transport.produce({
+      rtpParameters,
+      appData: {
+        source,
+      },
+      kind:
+        source === "microphone" || source === "screenshare-audio"
+          ? "audio"
+          : "video",
+    });
+    this.producers[producer.id] = producer;
+    return producer;
+  }
+
+  async consume(sourceFilter: string, participant: Participant) {
+    const transport = this.transports.recv;
+
+    const producers = Object.values(participant.consumers).filter((producer) =>
+      producer.appData.source.includes(sourceFilter),
+    );
+
+    const consumers: SFUConsumer[] = [];
+
+    for (const producer of producers) {
+      const canConsume = this.room.router.canConsume({
+        producerId: producer.id,
+        rtpCapabilities: participant.rtpCapabilities!,
+      });
+      if (!canConsume) {
+        console.log(
+          `sfu: producer ${producer.id} cannot be consumed by participant ${participant.id}`,
+        );
+        continue;
+      }
+
+      const consumer = await transport.consume({
+        producerId: producer.id,
+        rtpCapabilities: participant.rtpCapabilities!,
+        appData: {
+          source: producer.appData.source,
+          participantID: participant.id,
+        },
+      });
+      this.consumers[consumer.id] = consumer;
+      consumers.push(consumer);
+    }
+
+    return consumers;
+  }
+
+  resumeConsumer(consumerID: string) {
+    const consumer = this.consumers[consumerID];
+    if (!consumer) {
+      return;
+    }
+    consumer.resume();
+  }
+
+  closeProducers(producerIDs: string[]): string[] {
+    const closedProducers = [];
+    for (const producerID of producerIDs) {
+      const producer = this.producers[producerID];
+      if (!producer) {
+        continue;
+      }
+      this.closeProducer(producer);
+      closedProducers.push(producerID);
+    }
+    for (const producerID of closedProducers) {
+      delete this.producers[producerID];
+    }
+    return closedProducers;
+  }
+
+  closeConsumers(consumerIDs: string[]): string[] {
+    const closedConsumers = [];
+    for (const consumerID of consumerIDs) {
+      const consumer = this.consumers[consumerID];
+      if (!consumer) {
+        continue;
+      }
+      consumer.close();
+      closedConsumers.push(consumerID);
+    }
+    for (const consumerID of closedConsumers) {
+      delete this.consumers[consumerID];
+    }
+    return closedConsumers;
+  }
+
+  close() {
+    for (const producer of Object.values(this.producers)) {
+      this.closeProducer(producer);
+    }
+
+    for (const consumer of Object.values(this.consumers)) {
+      consumer.close();
+    }
+
+    for (const transport of Object.values(this.transports)) {
+      transport.close();
+    }
+  }
+
+  private closeProducer(producer: SFUProducer) {
+    producer.close();
+
+    for (const participant of Object.values(this.room.participants)) {
+      const closedConsumers = [];
+      for (const consumer of Object.values(participant.consumers)) {
+        if (consumer.producerId !== producer.id) {
+          continue;
+        }
+        consumer.close();
+        closedConsumers.push(consumer.id);
+      }
+      for (const consumerID of closedConsumers) {
+        delete participant.consumers[consumerID];
+      }
+    }
+  }
+}
---
+++ sfu-server/src/sfu.ts
@@ -0,0 +1,225 @@
+import os from "os";
+import mediasoup from "mediasoup";
+import { Worker, Router } from "mediasoup/node/lib/types.js";
+import {
+  ConnectTransportOptions,
+  ConsumeOptions,
+  InitOptions,
+  ProduceTransportOptions,
+  Room,
+} from "./types.js";
+import { Participant } from "./participant.js";
+
+export class SFU {
+  workerIdx = 0;
+  workers: Worker[] = [];
+
+  // maps worker pid to router
+  routers: Record<string, Router> = {};
+
+  rooms: Record<number, Room> = {};
+
+  options: InitOptions;
+
+  constructor(options: InitOptions) {
+    this.options = options;
+  }
+
+  async createWorkers() {
+    const numOfCPUs = os.cpus().length;
+    const workers: Worker[] = [];
+
+    for (let i = 0; i < numOfCPUs; i++) {
+      const worker = await mediasoup.createWorker(this.options.workerSettings);
+      const router = await worker.createRouter(this.options.routerOptions);
+      this.routers[worker.pid] = router;
+      workers.push(worker);
+    }
+
+    this.workers = workers;
+  }
+
+  createRoom(roomID: number) {
+    if (this.rooms[roomID]) {
+      return;
+    }
+
+    const worker = this.workers[this.workerIdx];
+    this.workerIdx = (this.workerIdx + 1) % this.workers.length;
+
+    const router = this.routers[worker.pid];
+    this.rooms[roomID] = {
+      router,
+      participants: {},
+    };
+  }
+
+  async addParticipant(id: string, roomID: number) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+
+    const sendTransport = await this.createTransport(room);
+    const recvTransport = await this.createTransport(room);
+
+    const participant = new Participant(
+      id,
+      {
+        recv: recvTransport,
+        send: sendTransport,
+      },
+      room,
+    );
+
+    room.participants[id] = participant;
+
+    return participant;
+  }
+
+  async removeParticipant(id: string, roomID: number) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+
+    const participant = room.participants[id];
+    if (!participant) {
+      return;
+    }
+
+    participant.close();
+
+    delete room.participants[id];
+  }
+
+  async connectTransport(
+    participantID: string,
+    roomID: number,
+    options: ConnectTransportOptions,
+  ) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+
+    const participant = room.participants[participantID];
+    if (!participant) {
+      return;
+    }
+
+    participant.connectTransport(options.direction, options.dtlsParameters);
+  }
+
+  async produceTransport(
+    participantID: string,
+    roomID: number,
+    options: ProduceTransportOptions,
+  ) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+    const participant = room.participants[participantID];
+    if (!participant) {
+      return;
+    }
+    const producer = await participant.produce(
+      options.source,
+      options.rtpParameters,
+    );
+    return producer;
+  }
+
+  async consume(
+    participantID: string,
+    roomID: number,
+    options: ConsumeOptions,
+  ) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+
+    const participant = room.participants[participantID];
+    if (!participant) {
+      return;
+    }
+
+    const remoteParticipant = room.participants[options.remoteParticipantID];
+    if (!remoteParticipant) {
+      return;
+    }
+
+    const consumers = await participant.consume(
+      options.sourceFilter,
+      remoteParticipant,
+    );
+    return consumers;
+  }
+
+  async closeProducers(
+    participantID: string,
+    roomID: number,
+    producerIDs: string[],
+  ) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+    const participant = room.participants[participantID];
+    if (!participant) {
+      return;
+    }
+    participant.closeProducers(producerIDs);
+  }
+
+  async closeConsumers(
+    participantID: string,
+    roomID: number,
+    consumerIDs: string[],
+  ) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+    const participant = room.participants[participantID];
+    if (!participant) {
+      return;
+    }
+    participant.closeConsumers(consumerIDs);
+  }
+
+  async resumeConsumer(
+    participantID: string,
+    roomID: number,
+    consumerID: string,
+  ) {
+    const room = this.rooms[roomID];
+    if (!room) {
+      return;
+    }
+    const participant = room.participants[participantID];
+    if (!participant) {
+      return;
+    }
+    participant.resumeConsumer(consumerID);
+  }
+
+  async deleteRooms(roomIDs: number[]) {
+    for (const roomID of roomIDs) {
+      const room = this.rooms[roomID];
+      if (!room) {
+        continue;
+      }
+      for (const participant of Object.values(room.participants)) {
+        participant.close();
+      }
+      delete this.rooms[roomID];
+    }
+  }
+
+  private async createTransport(room: Room) {
+    return room.router.createWebRtcTransport(this.options.transportOptions);
+  }
+}
